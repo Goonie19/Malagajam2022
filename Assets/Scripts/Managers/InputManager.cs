e331b711227b330fbcc3b1cf90f3cd7be994999a
Goonie19 <antonio.espinosabarrios98@gmail.com>
2022-01-28 20:35:55 +0100

    Input Manager now done well

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -1,18 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    public bool _keyBoardControls;
+    private GameActions _playerControls;
+    private PlayerInput _input;
+
+    private void Awake()
     {
-        
+        DontDestroyOnLoad(this);
+
+        _playerControls = new GameActions();
+        _playerControls.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    public GameActions PlayerControls
     {
-        
+        get { return _playerControls; }
+    }
+
+    #region Selector de Controles
+
+    public bool KeyBoardControls
+    {
+        get
+        {
+            return _keyBoardControls;
+        }
+    }
+
+    private void Update()
+    {
+        if (_input.currentControlScheme == _playerControls.KeyboardScheme.name)
+            _keyBoardControls = true;
+        else
+            _keyBoardControls = false;
+
+        //Debug.Log(_playerControls.Player.Movement.ReadValue<Vector2>() + " " + _input.currentControlScheme);
+
+
+    }
+
+    #endregion
+
+    private void OnEnable()
+    {
+
+        _input = GetComponent<PlayerInput>();
+    }
+
+    private void OnDisable()
+    {
+        _playerControls.Disable();
+    }
+
+    void Start()
+    {
+        _keyBoardControls = true;
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,62 +5,6 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    public bool _keyBoardControls;
-    private GameActions _playerControls;
-    private PlayerInput _input;
-
-    private void Awake()
-    {
-        DontDestroyOnLoad(this);
-
-        _playerControls = new GameActions();
-        _playerControls.Enable();
-    }
-
-    public GameActions PlayerControls
-    {
-        get { return _playerControls; }
-    }
-
-    #region Selector de Controles
-
-    public bool KeyBoardControls
-    {
-        get
-        {
-            return _keyBoardControls;
-        }
-    }
-
-    private void Update()
-    {
-        if (_input.currentControlScheme == _playerControls.KeyboardScheme.name)
-            _keyBoardControls = true;
-        else
-            _keyBoardControls = false;
-
-        //Debug.Log(_playerControls.Player.Movement.ReadValue<Vector2>() + " " + _input.currentControlScheme);
-
-
-    }
-
-    #endregion
-
-    private void OnEnable()
-    {
-
-        _input = GetComponent<PlayerInput>();
-    }
-
-    private void OnDisable()
-    {
-        _playerControls.Disable();
-    }
-
-    void Start()
-    {
-        _keyBoardControls = true;
-    }
+    
 
 }
