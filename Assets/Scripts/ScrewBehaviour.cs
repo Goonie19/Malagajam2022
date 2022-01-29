aa4a940352da7eee887bdd3fe7a7f41ec7906ef6
josioque <98118443+jobaDev@users.noreply.github.com>
2022-01-29 13:29:07 +0100

    Tornillo script updated

--- Assets/Scripts/ScrewBehaviour.cs
+++ Assets/Scripts/ScrewBehaviour.cs
@@ -4,55 +4,19 @@ using UnityEngine;
 
 public class ScrewBehaviour : MonoBehaviour
 {
-    public float moveSpeed;
-
-    private bool _isAtracted = false;
-    private GameObject magnet;
-    private Rigidbody2D _rb;
-    private CircleCollider2D _col;
-
-    private void Start()
-    {
-        _rb = gameObject.GetComponent<Rigidbody2D>();
-        _col = gameObject.GetComponent<CircleCollider2D>();
-    }
-
-    private void Update()
-    {
-        if (_isAtracted)
-        {
-            MoveToTheMagnet(magnet.transform);
-        }
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.CompareTag("MagneticObject"))
+        if (collision.gameObject.CompareTag("MagneticObject"))
         {
-            DesactivatePhysics();
-            SetChildOfTheMagnet(collision.gameObject);
-            //_isAtracted = true;
-            magnet = collision.gameObject;
+            Destroy(gameObject);
+        }
+        else if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
         }
         else if (collision.gameObject.CompareTag("Edge"))
         {
             Destroy(gameObject);
         }
-    }
-
-    private void MoveToTheMagnet(Transform target)
-    {
-        Vector3.MoveTowards(transform.position, target.position, moveSpeed * 100 * Time.deltaTime);
-    }
-
-    private void SetChildOfTheMagnet(GameObject magnet)
-    {
-        gameObject.transform.SetParent(magnet.transform);
-    }
-
-    private void DesactivatePhysics()
-    {
-        _rb.bodyType = RigidbodyType2D.Static;
-        _col.enabled = false;
     }
 }
